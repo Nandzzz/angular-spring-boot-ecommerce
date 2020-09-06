9e9e913e03e9d3f01a5f4eeaf73acf7c95d46c8e
Nandakumar Mudaliar <giri.nick.nandakumar@gmail.com>
2020-09-06 18:19:37 +0530

    added angular part

---
+++ frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {  //All the fields should match data From Spring Rest Api
+    id: string;
+    sku: string;
+    name:string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: string;
+    unitsInStock: string;
+    dateCreated: string;
+    lastUpdated: string;
+
+}
